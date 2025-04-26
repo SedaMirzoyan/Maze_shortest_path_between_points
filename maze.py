80f38551f402fb24f6669457daeefda9e63e226d
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-04-26 12:39:26 +0400

    Add files via upload

--- maze.py
+++ maze.py
@@ -34,7 +34,7 @@ def generate_maze(board_length):
     visited = []
 
     start_coords = [1, 1]
-    maze[start_coords[1]][start_coords[0]] = 0
+    maze[start_coords[0]][start_coords[1]] = 0
     stack.append(start_coords)
     visited.append(start_coords)
     current_coords = start_coords
@@ -50,12 +50,11 @@ def generate_maze(board_length):
             wall_x = start_coords[0] + int(random_pos[0]/2)
             wall_y = start_coords[1] + int(random_pos[1]/2)
             current_coords = [point_x, point_y] 
-            wall_coords = [wall_x, wall_y]
 
             is_valid_dir_point = is_valid_coords(current_coords, board_length)
             if(is_valid_dir_point and current_coords not in visited): 
-                maze[point_y][point_x] = 0
-                maze[wall_y][wall_x] = 0
+                maze[point_x][point_y] = 0
+                maze[wall_x][wall_y] = 0
 
                 visited.append(current_coords)
                 stack.append(current_coords)
@@ -65,94 +64,91 @@ def generate_maze(board_length):
         if(flag == False):
             stack.pop()
             
-    coord = set_letters(maze, board_length)
+    coord = set_points(maze, board_length)
         
     return (maze, coord)
 
 
-def set_letters(maze, board_length):
+def set_points(maze, board_length):
     coords = []
     for i in range(board_length):
         for j in range(board_length):
             if(maze[i][j] == 0):
                 coords.append((i, j))
         
-    random_coord = random.sample(coords, 2)
-    start_x, start_y = random_coord[0][0], random_coord[0][1]
-    end_x, end_y = random_coord[1][0], random_coord[1][1]
-    print("rc ", random_coord)
-    maze[start_y][start_x] = 2
-    #maze[start_y][start_x] = 'S'
-    maze[end_y][end_x] = 3
-    #maze[end_y][end_x] = 'E'
+    random_coords = random.sample(coords, 2)
+    start_x, start_y = random_coords[0][0], random_coords[0][1]
+    end_x, end_y = random_coords[1][0], random_coords[1][1]
+    print("rc ", random_coords)
+    maze[start_x][start_y] = 2
+    maze[end_x][end_y] = 3
 
-    return random_coord
+    return random_coords
 
 
     
 def is_valid_move(maze_and_coords, moves, board_length):
     maze, coords = maze_and_coords
-    start_y = coords[0][0]
-    start_x = coords[0][1]
+    start_x = coords[0][0]
+    start_y = coords[0][1]
 
     for move in moves.split():
         if move == "Left":
-            start_x = start_x - 1
-        elif move == "Right":
-            start_x = start_x + 1
-        elif move == "Up":
             start_y = start_y - 1
-        elif move == "Down":
+        elif move == "Right":
             start_y = start_y + 1
+        elif move == "Up":
+            start_x = start_x - 1
+        elif move == "Down":
+            start_x = start_x + 1
 
-    if  is_valid_coords([start_y, start_x], board_length)==False:
+    if  is_valid_coords([start_x, start_y], board_length)==False:
         return False
-    print("start_y",start_y)
-    print("start_x",start_x)
-    print(maze[start_x][start_y])
-    if maze[start_y][start_x] == 1:
+    if maze[start_x][start_y] == 1:
         return False
     
     return True
 
+
+
 def is_endpoint(maze_and_coords, moves):
     maze, coords = maze_and_coords
-    start_y = coords[0][0]
-    start_x = coords[0][1]
+    start_x = coords[0][0]
+    start_y = coords[0][1]
 
     end = (coords[1][0],coords[1][1])
     for move in moves.split():
         
         if move == "Left":
-            start_x = start_x - 1
-        elif move == "Right":
-            start_x = start_x + 1
-        elif move == "Up":
             start_y = start_y - 1
-        elif move == "Down":
+        elif move == "Right":
             start_y = start_y + 1
+        elif move == "Up":
+            start_x = start_x - 1
+        elif move == "Down":
+            start_x = start_x + 1
 
-    if ((start_y, start_x)) == end:
-        return False
+    if ((start_x, start_y)) == end:
+        return True
     
-    return True
+    return False
 
 
 def find_shortest_path(maze_and_coords, board_length):
     maze, coords = maze_and_coords
     
-    start_y, start_x = coords[0][0], coords[0][1]
-    end_y, end_x = coords[1][0], coords[1][1]
-    print(start_y, start_x)
+    start_x, start_y = coords[0][0], coords[0][1]
+    print("This is start", start_x, start_y)
+    end_x, end_y = coords[1][0], coords[1][1]
+    print(end_x, end_y)
     print("~~~~~~",  maze)
-    print("maze[start_y][start_x] = ", maze[start_x][start_y])
 
     q_moves = queue.Queue()
     q_moves.put("")
     direction = ""
     
     #counter = 0
-    while is_endpoint(maze_and_coords, direction):
+    while not is_endpoint(maze_and_coords, direction):
         direction = q_moves.get()
         print("~!!!!!!!!", direction)
         for prev_dir in ["Left", "Right", "Up", "Down"]:
@@ -161,6 +157,8 @@ def find_shortest_path(maze_and_coords, board_length):
             if is_valid_move(maze_and_coords, current_dir, board_length):
                 q_moves.put(current_dir)
 
+    print("final", direction)
+    print()
         #counter += 1
         #if counter >= 30:
         #    break
@@ -173,8 +171,8 @@ def print_maze(maze_and_coords):
     maze, coords = maze_and_coords
     for row in maze:
         print("".join('#' if cell == 1 
-                      else 'S' if cell == 2 #'S' #2 
-                      else 'E' if cell == 3 #'E' #3 
+                      else 'S' if cell == 2  
+                      else 'E' if cell == 3 
                       else  bcolors.FAIL + '.' + bcolors.ENDC if cell == 4 
                       else  ' ' for cell in row))
 
